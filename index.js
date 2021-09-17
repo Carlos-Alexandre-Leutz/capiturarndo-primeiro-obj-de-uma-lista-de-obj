ea801c19d69928bc452c68147e7cbac6881a1a7f
Carlos Alexandre Leutz <54986289+Carlos-Alexandre-Leutz@users.noreply.github.com>
2021-09-17 17:45:14 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 var obj = { first: 'someVal' };
 obj[Object.keys(obj)[0]]; //returns 'someVal'
+
+//Object.entries separa os atibutos do array em um arra
+
+let ObjDaTraducao = {
+    teste: "qwe",
+    coiso: "coiso",
+    asd:"Asd",
+    index:""
+}
+let array = Object.entries(ObjDaTraducao);
+array.forEach(index =>{
+    console.log("a", index)
+
+})
